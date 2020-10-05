955e61e232d8b2720a79476b507023f76780f9af
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2020-10-05 12:32:24 +0500

    #2

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <MainTour/>
+  </div>
+</template>
+
+<script>
+import MainTour from './components/MainTour.vue';
+
+export default {
+  name: 'App',
+  components: { MainTour },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/MainTour.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <header class="header">
+    <div class="wrapper">
+    <div class="burger__header">
+      <img src="../assets/img/burger.png" alt="burger">
+    </div>
+    </div>
+  </header>
+  <main>
+    <div class="main__title">
+      <h1>Горящие туры</h1>
+    </div>
+    <div class="rectangle">
+      <div>
+        <a href="#"><img src="../assets/img/rectangle.png" alt="">Испания</a>
+      </div>
+      <div>
+        <a href="#"><img src="../assets/img/rectangle2.png" alt="">Италия</a>
+      </div>
+      <div>
+        <a href="#"><img src="../assets/img/rectangle2.png" alt="">Исландия</a>
+      </div>
+      <div>
+        <a href="#"><img src="../assets/img/rectangle2.png" alt="">Тупция</a>
+      </div>
+    </div>
+    <div class="main__image block-1">
+      <div>
+        <img src="../assets/img/1.png" alt="italic">
+      </div>
+      <div class="box__price">
+        <img src="../assets/img/box-price.png" alt="">
+        <p>от 118 112 <span>₽</span></p>
+      </div>
+    </div>
+  </main>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MainTour',
+};
+</script>
+
+<style scoped>
+
+</style>
